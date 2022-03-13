8a5c9db3134f18b3ca76c5601fa8e8fe8f3e32a9
roymw09 <roymw09@gmail.com>
2022-03-13 12:40:13 -0700

    Minor change to Repository class

--- src/main/java/org/ac/cst8277/williams/roy/repository/SubscribedToRepository.java
+++ src/main/java/org/ac/cst8277/williams/roy/repository/SubscribedToRepository.java
@@ -8,10 +8,10 @@ import reactor.core.publisher.Flux;
 
 public interface SubscribedToRepository extends ReactiveCrudRepository<SubscribedTo, Integer> {
     // find all publishers a user is subscribed to
-    @Query("SELECT publisher_id, subscriber_id FROM subscribed_to WHERE subscriber_id = :subscriberId")
+    @Query("SELECT * FROM subscribed_to WHERE subscriber_id = :subscriberId")
     Flux<SubscribedTo> findAllPublishers(@Param("subscriberId") Integer subscriberId);
 
     // find all subscribers who are subscribed to a publisher
-    @Query("SELECT subscriber_id, publisher_id FROM subscribed_to WHERE publisher_id = :publisherId")
+    @Query("SELECT * FROM subscribed_to WHERE publisher_id = :publisherId")
     Flux<SubscribedTo> findAllSubscribers(@Param("publisherId") Integer publisherId);
 }
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,12 +1,12 @@
+DROP TABLE IF EXISTS subscriber;
 CREATE TABLE subscriber (
     id SERIAL PRIMARY KEY,
     user_id INT NOT NULL,
-    PRIMARY KEY (id)
 );
+DROP TABLE IF EXISTS subscribed_to;
 CREATE TABLE subscribed_to (
     id SERIAL PRIMARY KEY,
     subscriber_id INT NOT NULL,
     publisher_id INT NOT NULL,
-    PRIMARY KEY (id),
     FOREIGN KEY (subscriber_id) REFERENCES subscriber(id)
 );
