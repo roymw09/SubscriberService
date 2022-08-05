495f7a646d300c9a2d746443a7067d48d59d8768
roywilliams <roymw09@gmail.com>
2022-08-04 22:16:29 -0230

    Removed code for JWT validation

--- src/main/java/org/ac/cst8277/williams/roy/controller/SubscriberController.java
+++ src/main/java/org/ac/cst8277/williams/roy/controller/SubscriberController.java
@@ -1,17 +1,11 @@
 package org.ac.cst8277.williams.roy.controller;
 
-import org.ac.cst8277.williams.roy.model.JwtRequest;
-import org.ac.cst8277.williams.roy.model.JwtResponse;
 import org.ac.cst8277.williams.roy.model.Subscriber;
-import org.ac.cst8277.williams.roy.model.User;
 import org.ac.cst8277.williams.roy.service.SubscriberService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.client.RestTemplate;
 import reactor.core.publisher.Mono;
 
 @RestController
@@ -24,48 +18,14 @@ public class SubscriberController {
     @PostMapping("/create")
     @ResponseStatus(HttpStatus.CREATED)
     public ResponseEntity<Subscriber> createSubscriber(@RequestBody Subscriber subscriber) {
-        Integer userId = subscriber.getUser_id();
-        String username = new RestTemplate().getForObject("http://localhost:8081/users/user/getUsername/" + userId, String.class);
-        JwtRequest tokenRequest = new JwtRequest(username, "password");
-        tokenRequest.setUser_id(userId);
-        HttpEntity<JwtRequest> jwtRequestEntity = new HttpEntity<>(tokenRequest);
-        ResponseEntity<Subscriber> responseEntity;
-        try {
-            ResponseEntity<JwtResponse> response = new RestTemplate().postForEntity(
-                    "http://localhost:8081/authenticate/subscriber", jwtRequestEntity, JwtResponse.class);
-            subscriber.setId(response.getBody().getToken());
-            subscriberService.createSubscriber(subscriber).subscribe();
-            responseEntity = new ResponseEntity<>(subscriber, response.getStatusCode());
-        } catch (HttpClientErrorException e) {
-            responseEntity = new ResponseEntity<>(null, e.getStatusCode());
-        }
-        return responseEntity;
+        subscriberService.createSubscriber(subscriber).subscribe();
+        return new ResponseEntity<>(subscriber, HttpStatus.CREATED);
     }
 
     @GetMapping("/{subscriberId}")
     public Mono<ResponseEntity<Subscriber>> findById(@PathVariable Integer subscriberId) {
         Mono<Subscriber> subscriber = subscriberService.findById(subscriberId);
         return subscriber.map(ResponseEntity::ok)
-                .defaultIfEmpty(ResponseEntity.notFound().build());
-    }
-
-    // check the users token through the UMS to verify that they have subscriber rights
-    @GetMapping("/verify/{email}/{token}")
-    public ResponseEntity<User> checkUserToken(@PathVariable("email") String email, @PathVariable("token") String token) {
-        ResponseEntity<User> restTemplate;
-        try {
-            restTemplate = new RestTemplate().getForEntity(
-                    "http://usermanagement-service:8081/users/" + email + "/" + token, User.class);
-        } catch (HttpClientErrorException e) {
-            restTemplate = ResponseEntity.status(e.getRawStatusCode()).headers(e.getResponseHeaders()).body(null);
-        }
-        return restTemplate;
-    }
-
-    @GetMapping("/getToken/{userId}")
-    public Mono<ResponseEntity<Subscriber>> getSubscriberToken(@PathVariable("userId") Integer userId) {
-        Mono<Subscriber> token = subscriberService.getSubscriberToken(userId);
-        return token.map(ResponseEntity::ok)
                 .defaultIfEmpty(ResponseEntity.notFound().build());
     }
 }
